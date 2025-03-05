30d61dabab97e1bf1153159cb514cecdcf0bd68e
Jaeddan Gamilla <145520863+jaeddang@users.noreply.github.com>
2025-03-05 14:00:46 -0600

    Update imdb_scraper.py

--- imdb_scraper.py
+++ imdb_scraper.py
@@ -44,38 +44,44 @@ def direct_movie_url(genre_url, selected_movie, movies):
     movie_link_extensions = soup.find_all('a', class_="ipc-lockup-overlay ipc-focusable")
 
     link_map = {}
-    for i in range(10):
+
+    num_movies = min(len(movies), len(movie_link_extensions))
+
+    for i in range(num_movies):  # Avoid IndexError
         link_map[movies[i]] = BASE_URL + movie_link_extensions[i].get('href')
-    
+
     if selected_movie in link_map:
         return link_map[selected_movie]
     else:
         print('Invalid Movie Selected!')
-        exit(2)
+        sys.exit(2)
 
 
-if __name__ == "__main__":
+
+def main(num_genres_to_display=None, selected_genre=None, selected_movie=None):
     link_map = get_genre_link()
-
-    num_genres_to_display = input('How many genres are you interested in? (1 - 50): ')
-    num_genres_to_display = int(num_genres_to_display)
+    
+    if num_genres_to_display is None:
+        num_genres_to_display = input('How many genres are you interested in? (1 - 50): ')
+        num_genres_to_display = int(num_genres_to_display)
 
     if (num_genres_to_display > 50 or num_genres_to_display < 1):
         print('Please pick a number in the range 1 - 50!')
         exit(2)
 
     print("")
-    print('Available Genres:' + "")
-    genres = list(link_map.keys())
-    for i in range(int(num_genres_to_display)):
-        print(genres[i])
     
-    print("")
-    selected_genre = input('Select a genre above: ')
-    print("")
+    if selected_genre is None:
+        print('Available Genres:' + "")
+        genres = list(link_map.keys())
+        for i in range(int(num_genres_to_display)):
+            print(genres[i])
+    
+        print("")
+        selected_genre = input('Select a genre above: ')
+        print("")
 
     genre_url = link_map.get(selected_genre)
-
     if genre_url is None:
         print('Invalid Genre Selected!')
         exit(2)
@@ -88,10 +94,16 @@ if __name__ == "__main__":
             k += 1
     
     print("")
-    selected_movie = input('Type in a movie of your choice to receive the direct URL!: ')
+    if selected_movie is None:
+        selected_movie = input('Type in a movie of your choice to receive the direct URL!: ')
+    
     selected_movie_url = direct_movie_url(genre_url, selected_movie, movies)
 
     print("")
     print('Here is your URL! Enjoy watching ' + selected_movie + "!" + "")
     print(selected_movie_url)
+
+
+if __name__ == "__main__":
+    main()
     
